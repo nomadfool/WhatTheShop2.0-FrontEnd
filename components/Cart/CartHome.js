15e89065cf6394531f79a40197410323901d3906
lilyBrainy <m.o.kh.alenzii@gmail.com>
2019-03-28 20:35:44 +0300

    basic cart functionality

--- components/Cart/CartHome.js
+++ components/Cart/CartHome.js
@@ -14,20 +14,52 @@ import {
   Icon,
   Left,
   Body,
-  Right
+  Right,
+  Fab
 } from "native-base";
 import CartItem from "./CartItem";
+import Store from "../../stores/authStore";
 
 class CartHome extends Component {
+  handleAddingToCart = () => {
+    console.log(Store.user);
+    if (Store.user) {
+      if (cartStore.items) {
+        alert("thank you for shopping");
+      }
+    } else {
+      this.props.navigation.navigate("ProfileTab");
+    }
+  };
+
   render() {
+    let cartItems;
     if (cartStore.items) {
-      return (cartItems = cartStore.items.map(item => {
+      cartItems = cartStore.items.map(item => {
         return <CartItem key={item.id} item={item} />;
-      }));
+      });
     }
     return (
       <Container>
         <Content>{cartItems}</Content>
+        <Fab
+          Button
+          direction="right"
+          position="bottomRight"
+          style={{ backgroundColor: "#02c39a" }}
+          onPress={this.handleAddingToCart}
+        >
+          <Icon name="check-circle" type="FontAwesome" />
+          <Button style={{ backgroundColor: "#o2c39a" }}>
+            <Icon name="logo-whatsapp" />
+          </Button>
+          <Button style={{ backgroundColor: "#3B5998" }}>
+            <Icon name="logo-facebook" />
+          </Button>
+          <Button disabled style={{ backgroundColor: "#DD5144" }}>
+            <Icon name="mail" />
+          </Button>
+        </Fab>
       </Container>
     );
   }
