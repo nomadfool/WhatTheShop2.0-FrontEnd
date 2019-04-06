54a0a27abbdc60942c60b9d7e97284b2025e3a7b
NomadFool <nomad.fool@gmail.com>
2019-04-06 17:28:06 +0300

    list and detail view tweeks

--- components/Home/DetailItem.js
+++ components/Home/DetailItem.js
@@ -1,84 +1,90 @@
-import React, { Component } from "react";
-import { Image } from "react-native";
+import React, { Component } from 'react';
+import { Image } from 'react-native';
 
 import {
-  Container,
-  Header,
-  Content,
-  Card,
-  CardItem,
-  Thumbnail,
-  Text,
-  Button,
-  Icon,
-  Left,
-  Body,
-  Right,
-  Label
-} from "native-base";
+	Container,
+	Header,
+	Content,
+	Card,
+	CardItem,
+	Thumbnail,
+	Text,
+	Button,
+	Icon,
+	Left,
+	Body,
+	Right,
+	Label
+} from 'native-base';
 
-import cartStore from "../../stores/CartStore";
-import Store from "../../stores/authStore";
+import cartStore from '../../stores/CartStore';
+import Store from '../../stores/authStore';
 
 class DetailItem extends Component {
-  state = {
-    quantity: 1
-  };
-  handleAddingToCart = () => {
-    const quantity = this.state.quantity;
-    if (Store.user) {
-      const item = this.props.navigation.getParam("item");
-      cartStore.addItemsToCart(item, quantity);
-    } else {
-      alert("you have to login first to add things to cart");
-      this.props.navigation.navigate("ProfileTab");
-    }
-  };
+	state = {
+		quantity : 1
+	};
+	handleAddingToCart = () => {
+		const quantity = this.state.quantity;
+		if (Store.user) {
+			const item = this.props.navigation.getParam('item');
+			cartStore.addItemsToCart(item, quantity);
+		} else {
+			alert('you have to login first to add things to cart');
+			this.props.navigation.navigate('ProfileTab');
+		}
+	};
 
-  render() {
-    item = this.props.navigation.getParam("item");
-    return (
-      <Card>
-        <CardItem>
-          <Left>
-            {/* <Image
+	render() {
+		item = this.props.navigation.getParam('item');
+		return (
+			<Card
+				style={{
+					flex       : 1,
+					alignItems : 'center'
+				}}
+			>
+				<CardItem>
+					<Left>
+						{/* <Image
               source={{ uri: item.image }}
               style={{ height: 150, width: null, flex: 1 }}
             /> */}
-            <Body>
-              <Text>{item.name}</Text>
-              {/* <Text note>${item.price}</Text> */}
-            </Body>
-          </Left>
-        </CardItem>
+						<Body>
+							<Text>{item.name}</Text>
+							{/* <Text note>${item.price}</Text> */}
+						</Body>
+					</Left>
+				</CardItem>
 
-        <CardItem cardBody>
-          <Image
-            source={{ uri: item.image }}
-            style={{ height: 200, width: null, flex: 1 }}
-          />
-        </CardItem>
-        <CardItem>
-          <Body>
-            <Text>{item.description}</Text>
+				<CardItem cardBody>
+					<Image source={{ uri: item.image }} style={{ height: 300, width: '95%' }} />
+				</CardItem>
+				<CardItem>
+					<Body>
+						<Text>{item.description}</Text>
 
-            <Text note>${item.price}</Text>
-          </Body>
-        </CardItem>
+						<Text note>${item.price}</Text>
+					</Body>
+				</CardItem>
 
-        <CardItem>
-          <Button
-            onPress={() => this.setState({ quantity: this.state.quantity + 1 })}
-          >
-            <Text>+</Text>
-          </Button>
-          <Label>{this.state.quantity}</Label>
-          <Button
-            onPress={() => this.setState({ quantity: this.state.quantity - 1 })}
-          >
-            <Text>-</Text>
-          </Button>
-          {/* <Form>
+				<CardItem>
+					<Button
+						transparent
+						style={{ marginHorizontal: 5, fontSize: 30 }}
+						onPress={() => this.setState({ quantity: this.state.quantity + 1 })}
+					>
+						<Text>+</Text>
+					</Button>
+					<Label>{this.state.quantity}</Label>
+					<Button
+						transparent
+						style={{ marginHorizontal: 5, fontSize: 30 }}
+						onPress={() => this.setState({ quantity: this.state.quantity - 1 })}
+					>
+						<Text>-</Text>
+					</Button>
+					{/* <Form>
             <Button transparent onPress={this.handleAddingToCart}>
               <Icon active type="MaterialIcons" name="add-shopping-cart" />
             </Button>
@@ -89,19 +95,19 @@ class DetailItem extends Component {
               <Icon active type="MaterialIcons" name="add-shopping-cart" />
             </Button>
           </Form> */}
-          <Button transparent onPress={this.handleAddingToCart}>
-            <Icon active type="MaterialIcons" name="add-shopping-cart" />
-          </Button>
-          <Button transparent>
-            <Icon active name="chatbubbles" />
-            <Text>4 Comments</Text>
-          </Button>
+					<Button transparent onPress={this.handleAddingToCart}>
+						<Icon active type="MaterialIcons" name="add-shopping-cart" />
+					</Button>
+					<Button transparent>
+						<Icon active name="chatbubbles" />
+						<Text>4 Comments</Text>
+					</Button>
 
-          <Body />
-        </CardItem>
-      </Card>
-    );
-  }
+					<Body />
+				</CardItem>
+			</Card>
+		);
+	}
 }
 
 export default DetailItem;
--- components/Home/itemCard.js
+++ components/Home/itemCard.js
@@ -1,48 +1,44 @@
-import React, { Component } from "react";
-import { Image, TouchableHighlight } from "react-native";
+import React, { Component } from 'react';
+import { Image, TouchableHighlight } from 'react-native';
 import {
-  Container,
-  Header,
-  Content,
-  Card,
-  CardItem,
-  Thumbnail,
-  Text,
-  Button,
-  Icon,
-  Left,
-  Body,
-  Right
-} from "native-base";
+	Container,
+	Header,
+	Content,
+	Card,
+	CardItem,
+	Thumbnail,
+	Text,
+	Button,
+	Icon,
+	Left,
+	Body,
+	Right
+} from 'native-base';
 
 class ItemCard extends Component {
-  render() {
-    const item = this.props.item;
-    return (
-      <Card>
-        <TouchableHighlight
-          onPress={() =>
-            this.props.navigation.navigate("Detail", {
-              item: item
-            })
-          }
-        >
-          <CardItem>
-            <Left>
-              <Image
-                source={{ uri: item.image }}
-                style={{ height: 150, width: null, flex: 1 }}
-              />
-              <Body>
-                <Text>{item.name}</Text>
-                <Text note>${item.price}</Text>
-              </Body>
-            </Left>
-          </CardItem>
-        </TouchableHighlight>
-      </Card>
-    );
-  }
+	render() {
+		const item = this.props.item;
+		return (
+			<Card>
+				<TouchableHighlight
+					onPress={() =>
+						this.props.navigation.navigate('Detail', {
+							item : item
+						})}
+				>
+					<CardItem>
+						<Left>
+							<Image source={{ uri: item.image }} style={{ height: 100, width: '40%', flex: 1 }} />
+							<Body>
+								<Text>{item.name}</Text>
+								<Text note>${item.price}</Text>
+							</Body>
+						</Left>
+					</CardItem>
+				</TouchableHighlight>
+			</Card>
+		);
+	}
 }
 
 export default ItemCard;
